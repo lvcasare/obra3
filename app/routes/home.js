66472d06e9170f3be8b44edadee9d2394e818fa4
lvcasare <76891757+lvcasare@users.noreply.github.com>
2021-01-31 22:36:55 -0300

    Delete home.js

--- app/routes/home.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(application){
-    application.get('/', function (req,res){// pega url home
-        application.app.controllers.home.index(application,req,res);
-        
-        
-    });
-}
