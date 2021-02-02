8079f8d6248c02485e1053d450749e5306fc1026
lvcasare <76891757+lvcasare@users.noreply.github.com>
2021-02-02 17:11:54 -0300

    Create pedidos.js

---
+++ app/routes/pedidos.js
@@ -0,0 +1,15 @@
+module.exports = function (application){
+    
+
+    application.get('/pedidos', function (req,res){
+        application.app.controllers.pedidos.pedidos(application,req,res);
+        
+    });  
+
+    application.get('/pedidos/deletar', function (req,res){
+        application.app.controllers.pedidos.deletar(application,req,res);
+        
+    });  
+
+
+}
