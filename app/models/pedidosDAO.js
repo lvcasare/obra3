e5284d09d007b64d284387b7fc40125d925ae736
lvcasare <76891757+lvcasare@users.noreply.github.com>
2021-02-02 17:12:06 -0300

    Update pedidosDAO.js

--- app/models/pedidosDAO.js
+++ app/models/pedidosDAO.js
@@ -1,41 +1,38 @@
 function PedidosDAO(connection){
     this._connection = connection;
-
 }
-
-
 PedidosDAO.prototype.carregarPedido = function (pedidoid, callback){
     this._connection.query('INSERT INTO pedidos (fase) VALUES ("insert")');  
-    this._connection.query('SELECT * FROM pedidos ORDER BY id DESC LIMIT 1', callback);  
-    
-    
+    this._connection.query('SELECT * FROM pedidos ORDER BY id DESC LIMIT 1', callback);   
 }
-
-
-PedidosDAO.prototype.carregarPedido2 = function (pedidoid,callback){   
-    
+PedidosDAO.prototype.carregarPedido2 = function (pedidoid,callback){       
     this._connection.query('SELECT * FROM pedidos WHERE id = '+ pedidoid, callback);  
 }
-
 PedidosDAO.prototype.adicionarNovo = function (pedido,callback){   
 
     this._connection.query('INSERT INTO produtos (nome) VALUES ('+pedido.nome+')', pedido);
     this._connection.query('INSERT INTO itens SET ?', pedido, callback);
-
-
 }
-PedidosDAO.prototype.get5UltimasNoticias = function(callback){
-    this._connection.query('SELECT * FROM noticias ORDER BY data_criacao DESC LIMIT 5', callback);
-
-}
-
 PedidosDAO.prototype.editarPedido = function(pedido, callback){
-    console.log(pedido.id);
     this._connection.query('UPDATE pedidos SET ? WHERE id = ' + pedido.id , pedido, callback);
-    
-    
-    //this._connection.query('INSERT INTO pedidos SET ?', pedido, callback);
 
+}
+PedidosDAO.prototype.salvarTotalPedido = function(pedidoid,totalpedido, callback){
+    
+    this._connection.query('UPDATE pedidos SET total="'+totalpedido+'" WHERE id = ' + pedidoid, callback);
+}
+
+PedidosDAO.prototype.salvarTotalPedido = function(pedidoid,totalpedido, callback){
+    
+    this._connection.query('UPDATE pedidos SET total="'+totalpedido+'" WHERE id = ' + pedidoid, callback);
+}
+PedidosDAO.prototype.carregarTodos = function (callback){  
+    this._connection.query('DELETE FROM pedidos WHERE fase = "insert"');     
+    this._connection.query('SELECT * FROM pedidos ORDER BY fase ASC', callback);  
+}
+
+PedidosDAO.prototype.deletar = function (id, callback){  
+    this._connection.query('DELETE FROM pedidos WHERE id = '+ id, callback);
 }
 
 
