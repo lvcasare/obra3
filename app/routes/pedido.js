19e2b8bb5037a35977268ad7a33c3689942a56e1
lvcasare <76891757+lvcasare@users.noreply.github.com>
2021-02-02 17:11:56 -0300

    Update pedido.js

--- app/routes/pedido.js
+++ app/routes/pedido.js
@@ -2,31 +2,35 @@ module.exports = function (application){
     
 
     application.get('/', function (req,res){
-        application.app.controllers.pedidos.pedido(application,req,res);
+        application.app.controllers.pedido.pedido(application,req,res);
         
     });
     application.get('/pedido', function (req,res){
-        application.app.controllers.pedidos.pedido(application,req,res);
+        application.app.controllers.pedido.pedido(application,req,res);
         
-    });
+    });  
 
     application.post('/pedido/adicionarnovo', function (req,res){
         
-        application.app.controllers.pedidos.adicionarnovo(application,req,res);
+        application.app.controllers.pedido.adicionarnovo(application,req,res);
         
     });
 
     application.post('/pedido/adicionarantigo', function (req,res){
         
-        application.app.controllers.pedidos.adicionarantigo(application,req,res);
+        application.app.controllers.pedido.adicionarantigo(application,req,res);
         
     });
 
     application.post('/pedido/editarpedido', function (req,res){
         
-        application.app.controllers.pedidos.editarpedido(application,req,res);
+        application.app.controllers.pedido.editarpedido(application,req,res);
         
     });
+    application.get('/pedido/deletar', function (req,res){
+        application.app.controllers.pedido.deletar(application,req,res);
+        
+    });  
 
 
 }
