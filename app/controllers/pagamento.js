c6f70169c6d67f2a9534a88dc7b9de553ab9c7c4
lvcasare <76891757+lvcasare@users.noreply.github.com>
2021-02-04 05:27:04 -0300

    Update pagamento.js

--- app/controllers/pagamento.js
+++ app/controllers/pagamento.js
@@ -7,7 +7,19 @@ module.exports.pagamento = function(application, req, res){
     
     var callback = function(erro,result){
             console.log(result);
-            res.render('pagamentos/pagamento.ejs',{pedidos:result});;
+            res.render('pagamentos/pagamento.ejs',{pagamentos:result,get:get});;
             };        
     pagamentoModel.listar(get.id, callback);
+}
+
+
+module.exports.adicionar = function(application, req, res){
+    var pagamento = req.body;
+    var get=req.query;
+    var connection = application.config.dbConnection();
+    var pagamentoModel = new application.app.models.pagamentosDAO(connection);
+    var callback = function(erro,result){
+        res.redirect('/pagamento?id='+get.id+'&total='+get.total);
+        };        
+        pagamentoModel.adicionar(pagamento,callback)
 }
