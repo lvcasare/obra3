cf0149d580f3c72543a934b812e54c439e18ca35
lvcasare <76891757+lvcasare@users.noreply.github.com>
2021-03-03 15:20:46 -0300

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -5,8 +5,8 @@ var mysql = require ('mysql');
 var connMysql = function(){
 
 return mysql.createConnection({    
-    host: 'Localhost',
-    user: 'newuser',
+    host: '52.67.220.198',
+    user: 'monty',
     password:'fjsjfhs@(ahd5157YTRcccGr',
     database: 'portal_noticias'
 });
